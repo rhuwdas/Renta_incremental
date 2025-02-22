ed72448a2da98aeb5893fbe4bc6f6d41e223e614
Chimil_Prog <118279137+rhuwdas@users.noreply.github.com>
2025-02-21 20:39:15 -0600

    Programa final

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n;
+        int n, sumaMes = 0;
         long valorMeces = 1000;
         System.out.println("introduce la cantidad de meces");
         n = sc.nextInt();
         for (int i = 0; i < n; i++) {
-            System.out.println("El valor del mes " + i + " es de " + valorMeces);
+            System.out.println("El valor del mes " + (i+1) + " es de " + valorMeces);
             valorMeces *= 2;
-
-
+            sumaMes += valorMeces;
         }
+        System.out.println("El coste total por " + n + " meces es de " +sumaMes);
+
     }
 }
